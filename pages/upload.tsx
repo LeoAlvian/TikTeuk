f282a92691c211b6d96cf51f003f9f7032de4167
Leo <alvianleo@gmail.com>
2022-08-18 17:26:00 +0700

    detail page, like and comment functionalities are done, working on user profiles

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -4,6 +4,7 @@ import { FaCloudUploadAlt } from 'react-icons/fa'
 import { MdDelete } from 'react-icons/md'
 import axios from 'axios'
 import { SanityAssetDocument } from '@sanity/client'
+import { BASE_URL } from '../utils'
 
 import useAuthStore from '../store/authStore'
 import { client } from '../utils/client'
@@ -63,7 +64,7 @@ const Upload = () => {
                 topic: category
             }
             
-            await axios.post('http://localhost:3000/api/post', document)
+            await axios.post(`${BASE_URL}/api/post`, document)
 
             router.push('/')
         }
